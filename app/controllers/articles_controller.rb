df12678ebfde897329a41a36ab844660c7239335
Muamer Ribica <muamer.ribica@gmail.com>
2014-02-14 11:47:49 +0100

    Add articles create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,4 +22,24 @@ class ArticlesController < ApplicationController
       format.html
     end
   end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      respond_to do |format|
+        format.html { redirect_to article_path(@article)}
+      end
+    else
+      respond_to do |format|
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
