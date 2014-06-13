37029b474a0260760bd3f76911debedd685a8fb9
Muamer Ribica <muamer.ribica@gmail.com>
2014-06-13 16:58:22 +0200

    Create comment and redirect

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.save
+    redirect_to @comment.article
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :article_id)
+  end
+end
